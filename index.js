7e577aae1ef467886dbdea88f5dd849b850d8892
edsay12 <edvandearaujo2@hotmail.com>
2020-10-21 17:37:47 -0300

    carinho de compras

--- index.js
+++ index.js
@@ -26,3 +26,18 @@ $('.carousel').carousel({
          });
     });
    
+// carouceu
+
+$('#myCarousel').carousel({
+   interval: 10000
+ })
+ 
+ $('.carousel .item').each(function(){
+   var next = $(this).next();
+   if (!next.length) {
+     next = $(this).siblings(':first');
+   }
+   
+   next.children(':first-child').clone().appendTo($(this));
+ });
+
